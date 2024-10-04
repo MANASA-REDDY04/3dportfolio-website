523f379ab8c51e41c110998d39d5c2535fb225ef
Manasa Reddy <kandadimanasareddy12@gmail.com>
2024-10-04 14:31:46 +0530

    initial

---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+  bg: "#080707",
+  bgLight: "#31363F",
+  primary: "#FF6500",
+  text_primary: "#EFE1D1",
+  text_secondary: "#ed9f4c",
+  card: "#024242",
+  card_light: "#34e0e0",
+  button: "#34e0e0",
+  white: "#FFFFFF",
+  black: "#000000",
+};
+
+export const lightTheme = {
+  bg: "#FFFFFF",
+  bgLight: "#f0f0f0",
+  primary: "#be1adb",
+  text_primary: "#111111",
+  text_secondary: "#48494a",
+  card: "#FFFFFF",
+  button: "#5c5b5b",
+};
